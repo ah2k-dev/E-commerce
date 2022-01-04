10e17d4012b08c436630d6123ff9a645f3413aaa
ahmedhassan2k <ah2k.dev@gmail.com>
2022-01-05 02:57:22 +0500

    Error Handling
        • created global error handler class in utils
        • created middleware for errors
        • catch async errors for async functions
        • Catch Unhandled Promise Rejection/ Uncaught exceptions and shut down the server
        • handled wrong mongodb id error

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,55 +1,47 @@
 const Product = require("../models/productModel");
+const ErrorHandler = require("../utils/errorHandler");
+const catchAsyncErrors = require("../middleware/catchAsyncErrors");
 
 //create product -admin
-exports.createProduct = async (req, res) => {
+exports.createProduct = catchAsyncErrors(async (req, res) => {
   const product = await Product.create(req.body);
   res.status(201).json({ success: true, product });
-};
+});
 //get all products
-exports.getAllProducts = async (req, res) => {
+exports.getAllProducts = catchAsyncErrors(async (req, res) => {
   const product = await Product.find();
 
   res.status(200).json({ success: true, product });
-};
+});
 //get product details
-exports.getProductDetails = async (req, res, next) => {
-  let product = Product.findById(req.params.id);
+exports.getProductDetails = catchAsyncErrors(async (req, res, next) => {
+  let product = await Product.findById(req.params.id);
   if (!product) {
-    res.status(500).json({
-      success: false,
-      message: "Product not found",
-    });
+    return next(new ErrorHandler("Product not found", 404));
   }
   res.status(200).json({ success: true, product });
-};
+});
 //update product -admin
-exports.updateProduct = async (req, res, next) => {
-  let product = Product.findById(req.params.id);
+exports.updateProduct = catchAsyncErrors(async (req, res, next) => {
+  let product = await Product.findById(req.params.id);
   if (!product) {
-    res.status(500).json({
-      success: false,
-      message: "Product not found",
-    });
+    return next(new ErrorHandler("Product not found", 404));
   }
-
   product = await Product.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
     runValidators: true,
     useFindAndModify: false,
   });
   res.status(200).json({ success: true, product });
-};
+});
 //delete productt -admin
-exports.deleteProduct = async (req, res, next) => {
-  let product = Product.findById(req.params.id);
+exports.deleteProduct = catchAsyncErrors(async (req, res, next) => {
+  let product = await Product.findById(req.params.id);
   if (!product) {
-    res.status(500).json({
-      success: false,
-      message: "Product not found",
-    });
+    return next(new ErrorHandler("Product not found", 404));
   }
   await product.remove();
   res
     .status(200)
     .json({ success: true, message: "Product Deleted Successfully" });
-};
+});
--- server/server.js
+++ server/server.js
@@ -1,16 +1,35 @@
 const app = require("./app");
 const dotenv = require("dotenv");
-const connectDB = require('./db/connectDB')
+const connectDB = require("./db/connectDB");
+
+//Handling Uncaught Exception
+process.on("uncaughtException", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log(`Shutting down the server due to Uncaught Exception`);
+  process.exit(1);
+});
+
 //config
 dotenv.config({ path: "server/config/config.env" });
 
-const port = process.env.PORT || 3000
-const start = () => {
-    try {
-        connectDB(process.env.MONGO_URL)
-        app.listen(port, () => console.log(`Server running on ${port}`))
-    } catch (error) {
-        console.log(error);
-    }
-}
-start()
+const port = process.env.PORT || 3000;
+
+connectDB(process.env.MONGO_URL);
+
+const server = app.listen(port, () => console.log(`Server running on ${port}`));
+// const start = () => {
+//     try {
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
+// start()
+
+//Unhandled Promise Rejections
+process.on("unhandledRejection", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log(`Shutting down the server due to unhandled promise rejection`);
+  server.close(() => {
+    process.exit(1);
+  });
+});
